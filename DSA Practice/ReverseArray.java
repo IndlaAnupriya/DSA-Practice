7f635aeaf3026c828b2e5cfb2985dde83f2fb5a9
IndlaAnupriya <indlaanupriya@gmail.com>
2025-07-09 19:44:33 +0530

    Initial commit: Added ReverseArray.java under Arrays folder

---
+++ DSA Practice/ReverseArray.java
@@ -0,0 +1,21 @@
+public class ReverseArray {
+    public static void reverseArray(int arr[]) {
+        // code here
+        int n=arr.length;
+        int i=0;
+        int j=n-1;
+        while(i<j){
+            int t=arr[i];
+            arr[i]=arr[j];
+            arr[j]=t;
+            i++;
+            j--;
+        }
+        public static void main(String args[]){
+            int arr[] = {1,2,3,4,5};
+            reverseArray(arr);
+            for(int x:arr)
+              System.out.print(x+" ");
+        }
+    }
+} 
