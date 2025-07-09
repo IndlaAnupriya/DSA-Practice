fd61e4153e76ade6febae23f32e095ca2cd30d36
IndlaAnupriya <indlaanupriya@gmail.com>
2025-07-09 20:34:50 +0530

    Update MaxMinElement.java

--- DSA Practice/MaxMinElement.java
+++ DSA Practice/MaxMinElement.java
@@ -1,29 +1,24 @@
 public class MaxMinElement {
-    public static void maxElement(int arr[]){
+    public static void maxMinElement(int arr[]){
         int i;
         int n=arr.length;
         int maximum=arr[0];
+        int minimum=arr[0];
         for(i=1;i<n;i++){
             if(arr[i]>maximum){
                 maximum=arr[i];
             }
-        }
-        System.out.println("Maximum Element is:"+maximum); 
-    } 
-    public static void minElement(int arr[]){
-        int i;
-        int n=arr.length;
-        int minimum=arr[0];
-        for(i=1;i<n;i++){
-            if(arr[i]<minimum){
+            if(arr[i]<min){
                 minimum=arr[i];
-            }
         }
+     }
+        System.out.println("Maximum Element is:"+maximum+"\n"+"Minimum Element is:"+minimum); 
+    } 
+    
     System.out.println("Miniimum Element is:"+minimum);
     }
     public static void main(String args[]){
         int arr[]=new int[]{3,2,1,56,10000,167};
-        maxElement(arr);
-        minElement(arr);
+        maxMinElement(arr);
     }
 }
